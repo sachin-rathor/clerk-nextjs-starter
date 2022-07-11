17b414e035126e6d2f3d218a6181d256372c01b2
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2022-07-11 12:00:02 +0530

    Update getAuthenticatedUserId.js

--- pages/api/getAuthenticatedUserId.js
+++ pages/api/getAuthenticatedUserId.js
@@ -2,6 +2,7 @@ import { withAuth } from "@clerk/nextjs/api";
 
 export default withAuth((req, res) => {
   const { sessionId, userId } = req.auth;
+  console.log(req.auth + ", " + sessionId + ", " + userId );
 
   if (!sessionId) {
     return res.status(401).json({ id: null });
